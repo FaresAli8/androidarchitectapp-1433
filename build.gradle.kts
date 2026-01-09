63d76a9b480f7a9c7d9fe4306ee32b1b60535e21
FaresAli8 <faresaligamal8@gmail.com>
2026-01-09 18:48:36 +0200

    chore: add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+}
+true // Return true for script
